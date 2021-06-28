39d0e48c2e1c2bfa3c186b34eb6e30174e39e4b3
saumavadas <saumavadas@gmail.com>
2021-06-28 22:04:22 +0530

    uploaded on 28jun 2021

---
+++ src/Controller/UsersController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Users;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpClient\HttpClient;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+
+class UsersController extends AbstractController
+{
+    /**
+     * @Route("/app/v1/", name="index")
+     */
+    public function index(): Response
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/UsersController.php'
+        ]);
+    }  
+
+    /**
+     * @Route("/app/v1/users/{userid}/weather", name="weather")
+     */
+    public function weather(Request $request): Response
+    {       
+        $userId = $request->get('userid');
+        $user  = $this->getDoctrine()->getRepository(Users::class)->findOneBy(['id' => $userId]);
+        
+        $httpClient = HttpClient::create();
+        $response = $httpClient->request('GET', 'http://api.openweathermap.org/data/2.5/weather?q='.$user->getCity().','.$user->getState().'&appid=58fe595a877c084fc797adfc56976096');
+
+        $content = $response->getContent();
+        $content = $response->toArray();
+
+        if (200 !== $response->getStatusCode()) 
+        {
+            // handle the HTTP request error (e.g. retry the request)
+        } 
+        else 
+        {
+            $content = $response->getContent();
+        }
+        return new JsonResponse(json_decode($content));
+    }
+}#End of the class
---
+++ src/Entity/Users.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UsersRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=UsersRepository::class)
+ */
+class Users
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $fristName;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $lastName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $city;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $state;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $country;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFristName(): ?string
+    {
+        return $this->fristName;
+    }
+
+    public function setFristName(string $fristName): self
+    {
+        $this->fristName = $fristName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getState(): ?string
+    {
+        return $this->state;
+    }
+
+    public function setState(string $state): self
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(string $country): self
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+    
+}
